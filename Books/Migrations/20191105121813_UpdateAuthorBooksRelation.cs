b7e1ab51e9d12f36f5164d89e92238fd6556e49f
Noel Lopes <noel@ipg.pt>
2019-11-05 12:20:16 +0000

    Added new migration for updating relations

---
+++ Books/Migrations/20191105121813_UpdateAuthorBooksRelation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Books.Migrations
+{
+    public partial class UpdateAuthorBooksRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                table: "Book");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                table: "Book",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                table: "Book");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Author_AuthorId",
+                table: "Book",
+                column: "AuthorId",
+                principalTable: "Author",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

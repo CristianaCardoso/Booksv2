5fccf66a26c5551a6bca2e4f18c6394431e1bdc4
Noel Lopes <noel@ipg.pt>
2019-11-05 10:58:57 +0000

    Added books model

---
+++ Books/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Books.Models {
+    public class Book {
+        public int BookId { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Title { get; set; }
+
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
